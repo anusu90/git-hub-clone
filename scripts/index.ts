bfae237b2a3af40d0c5836338b2cf439224281d1
anunay sinha <anusu90@gmail.com>
2020-12-16 12:24:50 +0530

    updated PAT

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,45 +3,35 @@ const { Octokit } = require("@octokit/core");
 // rajavasanthan@guvi.in
 
 
-async function searchGithub (){
+// async function searchGithub (){
 
-    const url = "https://api.github.com";
-    const request = await fetch(url);
-    const response = await request.json();
+//     const url = "https://api.github.com";
+//     const request = await fetch(url);
+//     const response = await request.json();
 
-    console.log(response);
-}
+//     console.log(response);
+// }
 
 // searchGithub ();
 
-// d5a9be1d108841736b180821d95cb1527f720d7d
+// 1f132192db69d44a6e0b484d87618838bc022ffc
 
 async function MyFunc (){
 
-
     try {
-        
-        const octokit = new Octokit({ auth: `d5a9be1d108841736b180821d95cb1527f720d7d` });
+        const octokit = new Octokit({ auth: `1f132192db69d44a6e0b484d87618838bc022ffc` });
         const response = await octokit.request('GET /users/{user}',{
             user: 'anusu900uy',
         });
         
-        
     } catch (error) {
         
-        
-        const octokit = new Octokit({ auth: `d5a9be1d108841736b180821d95cb1527f720d7d` });
+        const octokit = new Octokit({ auth: `1f132192db69d44a6e0b484d87618838bc022ffc` });
         const res2 = await octokit.request('GET /search/code', {
             q: 'guvi',
         });
-
-        
-
-        console.log(res2.data)
-        
+        console.log(res2.data)   
     }
-
-
 
 }
 
